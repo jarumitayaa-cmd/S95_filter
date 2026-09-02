0ad3046526abc7b9964eb770d854cd55bbfd11d1
jarumitayaa-cmd <jarumitayaa@gmail.com>
2026-09-02 23:47:19 +0530

    Update button URLs in pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1298,7 +1298,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('ᴅʀᴇᴀᴍxʙᴏᴛᴢ 📜', url='https://github.com/DreamXBotz/Auto_Filter_Bot.git'),
+            InlineKeyboardButton('🇦​​🇩​​🇲​​🇮​​🇳', url='https://t.me/Shyam95bot'),
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ ⇋', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
